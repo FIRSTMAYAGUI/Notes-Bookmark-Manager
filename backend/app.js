0dbe1cd708cadf6185e77e654bcaf54150a5d194
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-02-06 15:50:39 +0100

    setup neon database connection

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
+import 'dotenv/config.js';
 import express from "express";
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { getPgVersion } from "./config/db.js";
 
 const app = express();
 
 const APP_PORT = process.env.APP_PORT || 5000;
 
 app.listen(APP_PORT, ()=>{
+    console.log(getPgVersion());
     console.log("Server running at http://localhost:" + APP_PORT);
 });
---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import { neon } from '@neondatabase/serverless';
+
+const sql = neon(process.env.DATABASE_URL);
+
+export async function getPgVersion() {
+   try {
+        const result = await sql`SELECT version()`;
+        console.log(result[0]);
+    } catch (error) {
+        console.log('error', error.message)
+    }
+}
