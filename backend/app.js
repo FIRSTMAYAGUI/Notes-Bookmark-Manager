09876a029e8b3bbc8d74fdb0ca7311f58f336ed5
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-02-10 22:26:27 +0100

    Add createNote and getNotes logic

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import 'dotenv/config.js';
 import express from "express";
 import { getPgVersion } from "./config/db.js";
-import { createNotesTable } from './models/notes.js';
+import { createNotes, createNotesTable, getNotes } from './models/notes.js';
 
 const app = express();
+app.use(express.json());/* allows us to accept json data in the request body (in req.body). Without this
+you won't be able to do this "const { name, price, image } = req.body" */
 
 const APP_PORT = process.env.APP_PORT || 5000;
 
@@ -12,3 +14,22 @@ app.listen(APP_PORT, ()=>{
     createNotesTable();
     console.log("Server running at http://localhost:" + APP_PORT);
 });
+
+//without using app.use("/api/notes", notesRoutes);
+app.post('/api/notes', async (req, res) =>{
+    const { title, content } =  req.body; //data comming from the frontend
+    const tags = Array.isArray(req.body.tags) ? req.body.tags : [];
+
+    if (!title || !content){
+        return res.status(400).json({success: false, message: 'Please fill in all fields'});
+    }
+
+    try {
+        await createNotes(title, content, tags);
+        const notes = await getNotes();
+        return res.status(201).json({success: true, data: notes, message: 'Notes created successfully'});
+    } catch (error) {
+        console.error('Error:', error.message);
+        return res.status(500).json({success: false, message: 'Server Error'});
+    }
+})
--- backend/models/notes.js
+++ backend/models/notes.js
@@ -4,15 +4,15 @@ export const createNotesTable = async () => {
   try {
     await sql`
     CREATE TABLE IF NOT EXISTS notes (
-        id BIGSERIAL PRIMARY KEY,
+      id BIGSERIAL PRIMARY KEY,
 
-        title TEXT NOT NULL,
-        content TEXT NOT NULL,
-        tags TEXT[] DEFAULT '{}',
-        is_favorite BOOLEAN DEFAULT FALSE,
+      title TEXT NOT NULL,
+      content TEXT NOT NULL,
+      tags TEXT[] DEFAULT '{}',
+      is_favorite BOOLEAN DEFAULT FALSE,
 
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-        updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
     )
     `;
 
@@ -20,5 +20,30 @@ export const createNotesTable = async () => {
   } catch (error) {
     console.log('error:', error)
     return error.message
+  }
+}
+
+export const createNotes = async (title, content, tags = []) => {
+  try {
+    const createdNote = await sql`
+      INSERT INTO notes (title, content, tags)
+      VALUES (${title}, ${content}, ${tags}::text[]) 
+      RETURNING *;
+    `; //The ::text[] is to make postgre cast the tags as an array
+
+    return createdNote[0];
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export const getNotes = async () => {
+  try {
+    const notesList = await sql`
+      SELECT * FROM notes;
+    `;
+    return notesList;
+  } catch (error) {
+    return error.message;
   }
 }
